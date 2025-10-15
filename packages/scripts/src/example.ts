15d71ca028f709019fe3bc959af91246cbcabd87
Jure Jures <jure.04jj@gmail.com>
2025-10-15 15:06:15 +0200

    init commit

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@w-info-sst/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
