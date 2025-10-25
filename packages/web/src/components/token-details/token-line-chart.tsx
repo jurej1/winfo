7a46bd6be9a7b1c053bc2bcbe5400cea4effd7df
Jure Jures <jure.04jj@gmail.com>
2025-10-25 15:59:27 +0200

    adding some margin

--- packages/web/src/components/token-details/token-line-chart.tsx
+++ packages/web/src/components/token-details/token-line-chart.tsx
@@ -33,6 +33,7 @@ export function TokenLineChart({ data, timeframe }: Props) {
       });
     }
   };
+
   return (
     <div style={{ width: "100%", height: 400 }} className="p-4">
       <ResponsiveContainer width="100%" height="100%">
--- packages/web/src/pages/token-details-page.tsx
+++ packages/web/src/pages/token-details-page.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function TokenDetailsPage({ id }: Props) {
   return (
-    <div className="m-auto flex max-w-7xl flex-col">
+    <div className="m-auto my-2 flex max-w-7xl flex-col">
       <TokenChartCard id={id} />
       <TokenDetails className="mt-10" id={id} />
     </div>
