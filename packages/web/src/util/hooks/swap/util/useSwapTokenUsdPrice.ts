cc56fa8eff5e1c3953a0965198533444c1e346a2
Jure Jures <jure.04jj@gmail.com>
2025-10-29 18:30:14 +0100

    updating usdSwapTokenUSD price

--- packages/web/src/util/hooks/swap/util/useSwapTokenUsdPrice.ts
+++ packages/web/src/util/hooks/swap/util/useSwapTokenUsdPrice.ts
@@ -10,12 +10,12 @@ export const useSwapTokenUsdPrice = ({
   value,
 }: UseSwapTokenUsdPriceProps) => {
   return useMemo(() => {
-    if (!price || !value) return;
+    if (!price || !value) return "$0.0";
 
     const valNumber = Number(value);
     const tokenPrice = Number(price);
 
-    if (isNaN(valNumber) || isNaN(tokenPrice)) return;
+    if (isNaN(valNumber) || isNaN(tokenPrice)) return "$0.0";
 
     const totalUsd = valNumber * tokenPrice;
     return `$${totalUsd.toLocaleString(undefined, { maximumFractionDigits: 2 })}`;
