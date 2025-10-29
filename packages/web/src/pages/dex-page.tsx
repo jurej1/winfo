df0607e483d2ff62aeb9d5381b01ad6431a8f686
Jure Jures <jure.04jj@gmail.com>
2025-10-29 18:49:16 +0100

    fixes

--- packages/web/src/pages/dex-page.tsx
+++ packages/web/src/pages/dex-page.tsx
@@ -1,7 +1,7 @@
 import { DexRecentTransactions } from "@/components/dex";
 import { DexSwapCard } from "@/components/dex/dex-swap-card";
 
-export function DexPage() {
+export default function DexPage() {
   return (
     <div className="mx-auto my-3 flex max-w-7xl flex-col gap-2">
       <DexSwapCard />
