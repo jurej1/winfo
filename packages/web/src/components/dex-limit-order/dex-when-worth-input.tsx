45ae99ab86a0bbadf391e38a0d3da729ea5a8bfc
Jure Jures <jure.04jj@gmail.com>
2025-10-31 12:38:28 +0100

    dex correcting input

--- packages/web/src/components/dex-limit-order/dex-when-worth-input.tsx
+++ packages/web/src/components/dex-limit-order/dex-when-worth-input.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { DexCardBackground } from "../dex/dex-card-bacground";
 import { TokenDBwithPrice } from "@w-info-sst/db";
 import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
 
 type Props = {
   sellToken?: TokenDBwithPrice;
@@ -16,7 +17,22 @@ export function DexWhenWorthInput({ sellToken, buyToken }: Props) {
   return (
     <DexCardBackground isHovering={setIsHover}>
       <p className="text-xs">When 1 {sellToken?.symbol} is worth </p>
-      <Input readOnly />
+      <Input
+        // disabled={isLoadingTokens}
+        // key={token?.address}
+        // type={isNumberInput ? "number" : undefined}
+        className={cn(
+          "border-none text-black shadow-none focus-visible:border-none focus-visible:ring-0",
+          "transition-colors duration-300 ease-in-out",
+        )}
+        placeholder="0.0"
+        // value={value}
+        style={{
+          fontSize: 28,
+        }}
+        onChange={(event) => {}}
+        readOnly={false}
+      />
     </DexCardBackground>
   );
 }
