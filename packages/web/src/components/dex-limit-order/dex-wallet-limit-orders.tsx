841a5914807ceb273c703783f999ce7a898d7895
Jure Jures <jure.04jj@gmail.com>
2025-11-03 22:13:10 +0100

    d

--- packages/web/src/components/dex-limit-order/dex-wallet-limit-orders.tsx
+++ packages/web/src/components/dex-limit-order/dex-wallet-limit-orders.tsx
@@ -19,6 +19,7 @@ export function DexWalletLimitOrders() {
   const { isLoading, data } = useLimitOrderByWallet();
 
   const { address } = useAccount();
+
   return (
     <div className="flex flex-col gap-2">
       <div className="flex items-center justify-between">
