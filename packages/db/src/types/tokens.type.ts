5b51924c887fe071a268919415da0d9a74cb9e80
Jure Jures <jure.04jj@gmail.com>
2025-11-06 13:05:01 +0100

    adding gettokendbwithprice

--- packages/db/src/types/tokens.type.ts
+++ packages/db/src/types/tokens.type.ts
@@ -3,3 +3,7 @@ import { tokens } from "../schemas";
 export type InsertTokenDB = typeof tokens.$inferInsert;
 
 export type SelectTokenDB = typeof tokens.$inferSelect;
+
+export type TokenDBwithPrice = SelectTokenDB & {
+  priceUsd: number;
+};
